0ada6a8121739da2fa1689806b72b7ea60ce1f73
FFMG <github@myoddweb.com>
2018-04-04 19:18:05 +0200

    Added console project

---
+++ console/SQLiteServerConsole/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using SQLiteServer.Data.SQLiteServer;
+
+namespace SQLiteServerConsole
+{
+  internal class Program
+  {
+    private static readonly IPAddress Address = IPAddress.Parse("127.0.0.1");
+    private const int Port = 1100;
+    private const int Backlog = 500;
+    private const int HeartBeatTimeOut = 500;
+
+    private static void Main(string[] args)
+    {
+      var path = Directory.GetCurrentDirectory();
+      var source = Path.Combine(path, "sample.db");
+
+      Console.WriteLine( $"Db: {source}");
+
+      var connection = new SQLiteServerConnection($"Data Source={source};Version=3;", Address, Port, Backlog, HeartBeatTimeOut);
+      try
+      {
+        connection.Open();
+
+        Console.WriteLine("Press Exit to ... exit (duh)");
+        while (true)
+        {
+          var s = Console.ReadLine();
+          if (s == null)
+          {
+            continue;
+          }
+          if (s.ToLower() == "exit")
+          {
+            Console.WriteLine("Ok, bye");
+            break;
+          }
+        }
+
+        connection.Close();
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine( $"Unable to open '{source}', please check your permissions.");
+        Console.WriteLine( $"Error was : {e.Message}.");
+      }
+    }
+  }
+}
--- console/SQLiteServerConsole/Properties/AssemblyInfo.cs
+++ console/SQLiteServerConsole/Properties/AssemblyInfo.cs
@@ -1,33 +1,18 @@
-﻿//This file is part of SQLiteServer.
-//
-//    SQLiteServer is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
-//
-//    SQLiteServer is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with SQLiteServer.  If not, see<https://www.gnu.org/licenses/gpl-3.0.en.html>.
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SQLiteServer")]
+[assembly: AssemblyTitle("SQLiteServerConsole")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SQLiteServer")]
+[assembly: AssemblyProduct("SQLiteServerConsole")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: InternalsVisibleTo("SQLiteServer.Test")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -35,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("37fc701a-840c-4b5a-bead-996567eaa5ba")]
+[assembly: Guid("eae4b364-fb3b-4103-8def-0d485ced75e4")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -47,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
