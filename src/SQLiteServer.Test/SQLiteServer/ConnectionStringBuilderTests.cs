83453d342353e5dd5d2dd7da704412babbb8ed68
FFMG <github@myoddweb.com>
2018-04-12 21:16:41 +0200

    Added more tests + connection string values

--- src/SQLiteServer.Test/SQLiteServer/ConnectionStringBuilderTests.cs
+++ src/SQLiteServer.Test/SQLiteServer/ConnectionStringBuilderTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Data.SQLite;
+using NUnit.Framework;
 using SQLiteServer.Data.SQLiteServer;
 
 namespace SQLiteServer.Test.SQLiteServer
@@ -17,6 +19,295 @@ namespace SQLiteServer.Test.SQLiteServer
       var sql = new SQLiteServerConnectionStringBuilder("Data Source=:memory:");
       Assert.AreEqual(":memory:", sql["Data Source"]);
       Assert.AreEqual(":memory:", sql.DataSource);
+    }
+
+    [Test]
+    public void DefaultDataSource()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder();
+      Assert.AreEqual("", sql.DataSource);
+    }
+
+    [Test]
+    public void SetDataSource()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder
+      {
+        DataSource = "Blah"
+      };
+      Assert.AreEqual("Blah", sql.DataSource);
+    }
+
+    [Test]
+    public void DefaultUri()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder();
+      Assert.AreEqual(null, sql.Uri);
+    }
+
+    [Test]
+    public void SetUri()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder
+      {
+        Uri = "Bla"
+      };
+      Assert.AreEqual("Bla", sql.Uri);
+    }
+
+    [Test]
+    public void GivenUriInConnectionString()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder( "Uri=Bla" );
+      Assert.AreEqual("Bla", sql.Uri);
+    }
+
+    [Test]
+    public void DefaultFullUri()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder();
+      Assert.AreEqual(null, sql.FullUri);
+    }
+
+    [Test]
+    public void SetFullUri()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder
+      {
+        FullUri = "Bla"
+      };
+      Assert.AreEqual("Bla", sql.FullUri);
+    }
+
+    [Test]
+    public void GivenFullUriInConnectionString()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder("FullUri=Bla");
+      Assert.AreEqual("Bla", sql.FullUri);
+    }
+
+    [Test]
+    public void DefaultVersion()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder();
+      Assert.AreEqual(3, sql.Version);
+    }
+
+    [Test]
+    public void SetInvalidVersion()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder();
+      Assert.Throws<NotSupportedException>( () =>
+      {
+        sql.Version = 2;
+      });
+    }
+
+    [Test]
+    public void InvalidVersionGivenByConnectionString()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder("Version=2");
+      Assert.AreEqual(2, sql.Version);
+    }
+
+    [Test]
+    public void DefaultSynchronousMode()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder();
+      Assert.AreEqual(SynchronizationModes.Normal, sql.SyncMode);
+    }
+
+    [Test]
+    public void GivenSynchronousMode()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder("Synchronous=Full");
+      Assert.AreEqual(SynchronizationModes.Full, sql.SyncMode);
+    }
+
+    [Test]
+    public void CanChangeValueOfSynchronousMode()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder("Synchronous=Full");
+      Assert.AreEqual(SynchronizationModes.Full, sql.SyncMode);
+      sql.SyncMode = SynchronizationModes.Off;
+      Assert.AreEqual(SynchronizationModes.Off, sql.SyncMode);
+    }
+
+    [Test]
+    public void InvalidSynchronousModeIsDefault()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder("Synchronous=BlahBlah");
+      Assert.AreEqual(SynchronizationModes.Normal, sql.SyncMode);
+    }
+
+    [Test]
+    public void UseUTF16EncodingDefaultValue()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder();
+      Assert.IsFalse(sql.UseUTF16Encoding);
+    }
+
+    [Test]
+    public void GivenUseUTF16EncodingAsAString()
+    {
+      var sql1 = new SQLiteServerConnectionStringBuilder("useutf16encoding=false");
+      Assert.IsFalse(sql1.UseUTF16Encoding);
+
+      var sql2 = new SQLiteServerConnectionStringBuilder("useutf16encoding=true");
+      Assert.IsTrue(sql2.UseUTF16Encoding);
+    }
+
+    [Test]
+    public void SetUseUTF16EncodingAsAString()
+    {
+      var sql1 = new SQLiteServerConnectionStringBuilder("useutf16encoding=false");
+      Assert.IsFalse(sql1.UseUTF16Encoding);
+      sql1.UseUTF16Encoding = true;
+      Assert.IsTrue(sql1.UseUTF16Encoding);
+
+      var sql2 = new SQLiteServerConnectionStringBuilder("useutf16encoding=true");
+      Assert.IsTrue(sql2.UseUTF16Encoding);
+      sql2.UseUTF16Encoding = false;
+      Assert.IsFalse(sql2.UseUTF16Encoding);
+    }
+
+    [Test]
+    public void GivenUseUTF16EncodingAsANumber()
+    {
+      var sql1 = new SQLiteServerConnectionStringBuilder("useutf16encoding=0");
+      Assert.IsFalse(sql1.UseUTF16Encoding);
+
+      var sql2 = new SQLiteServerConnectionStringBuilder("useutf16encoding=1");
+      Assert.IsTrue(sql2.UseUTF16Encoding);
+
+      var sql3 = new SQLiteServerConnectionStringBuilder("useutf16encoding=1234");
+      Assert.IsTrue(sql3.UseUTF16Encoding);
+    }
+
+    [Test]
+    public void UPoolingDefaultValue()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder();
+      Assert.IsFalse(sql.Pooling);
+    }
+
+    [Test]
+    public void GivenPoolingAsAString()
+    {
+      var sql1 = new SQLiteServerConnectionStringBuilder("pooling=false");
+      Assert.IsFalse(sql1.Pooling);
+
+      var sql2 = new SQLiteServerConnectionStringBuilder("Pooling=true");
+      Assert.IsTrue(sql2.Pooling);
+    }
+
+    [Test]
+    public void SetPooling()
+    {
+      var sql1 = new SQLiteServerConnectionStringBuilder("pooling=false");
+      Assert.IsFalse(sql1.Pooling);
+      sql1.Pooling = true;
+      Assert.IsTrue(sql1.Pooling);
+
+      var sql2 = new SQLiteServerConnectionStringBuilder("Pooling=true");
+      Assert.IsTrue(sql2.Pooling);
+      sql2.Pooling = false;
+      Assert.IsFalse(sql2.Pooling);
+    }
+
+    [Test]
+    public void GivenPoolingAsANumber()
+    {
+      var sql1 = new SQLiteServerConnectionStringBuilder("Pooling=0");
+      Assert.IsFalse(sql1.Pooling);
+
+      var sql2 = new SQLiteServerConnectionStringBuilder("pooling=1");
+      Assert.IsTrue(sql2.Pooling);
+
+      var sql3 = new SQLiteServerConnectionStringBuilder("pooling=1234");
+      Assert.IsTrue(sql3.Pooling);
+    }
+
+    [Test]
+    public void GetTheDefaultTimeOutDefaultValue()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder();
+      Assert.AreEqual( 30, sql.DefaultTimeout);
+    }
+
+    [Test]
+    public void GetTheDefaultTimeOutGivenInConnectionString()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder("Default Timeout=60");
+      Assert.AreEqual(60, sql.DefaultTimeout);
+    }
+
+    [Test]
+    public void SetTheDefaultTimeOutGivenInConnectionString()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder("Default Timeout=60")
+      {
+        DefaultTimeout = 40
+      };
+      Assert.AreEqual(40, sql.DefaultTimeout);
+    }
+
+    [Test]
+    public void GetTheBusyTimeOutDefaultValue()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder();
+      Assert.AreEqual(0, sql.BusyTimeout);
+    }
+
+    [Test]
+    public void GetTheBusyTimeOutGivenInConnectionString()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder("BusyTimeout=60");
+      Assert.AreEqual(60, sql.BusyTimeout);
+    }
+
+    [Test]
+    public void SetTheBusyTimeOutGivenInConnectionString()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder("BusyTimeout=60")
+      {
+        BusyTimeout = 40
+      };
+      Assert.AreEqual(40, sql.BusyTimeout);
+    }
+
+    [Test]
+    public void IsNotReadOnlyByDefault()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder();
+      Assert.IsFalse(sql.ReadOnly);
+    }
+
+    [Test]
+    public void SetIsReadOnly()
+    {
+      var sql = new SQLiteServerConnectionStringBuilder {ReadOnly = true};
+      Assert.IsTrue(sql.ReadOnly);
+    }
+
+    [Test]
+    public void ReadOnlyGivenAsAString()
+    {
+      var sql1 = new SQLiteServerConnectionStringBuilder( "Read Only=true");
+      Assert.IsTrue(sql1.ReadOnly);
+
+      var sql2 = new SQLiteServerConnectionStringBuilder("Read Only=false");
+      Assert.IsFalse(sql2.ReadOnly);
+    }
+
+    [Test]
+    public void ReadOnlyGivenAsANumber()
+    {
+      var sql1 = new SQLiteServerConnectionStringBuilder("Read Only=0");
+      Assert.IsFalse(sql1.ReadOnly);
+
+      var sql2 = new SQLiteServerConnectionStringBuilder("Read Only=1");
+      Assert.IsTrue(sql2.ReadOnly);
     }
   }
 }
--- src/SQLiteServer/Data/SQLiteServer/SQLiteServerConnectionStringBuilder.cs
+++ src/SQLiteServer/Data/SQLiteServer/SQLiteServerConnectionStringBuilder.cs
@@ -15,8 +15,10 @@
 
 using System;
 using System.Collections;
-using System.ComponentModel;
 using System.Data.Common;
+using System.Data.SQLite;
+using System.Globalization;
+using static System.ComponentModel.TypeDescriptor;
 
 namespace SQLiteServer.Data.SQLiteServer
 {
@@ -25,17 +27,19 @@ namespace SQLiteServer.Data.SQLiteServer
   public sealed class SQLiteServerConnectionStringBuilder : DbConnectionStringBuilder
   {
     #region Private
+
     /// <summary>
     /// Properties of this class
     /// </summary>
-    private Hashtable _properties;
+    private readonly Hashtable _properties;
+
     #endregion
 
     /// <inheritdoc />
     /// <summary>
     /// Construnct with no connection string.
     /// </summary>
-    public SQLiteServerConnectionStringBuilder() : this( null )
+    public SQLiteServerConnectionStringBuilder() : this(null)
     {
     }
 
@@ -55,17 +59,220 @@ namespace SQLiteServer.Data.SQLiteServer
       }
     }
 
+    private const string KeyUseUtf16Encoding = "UseUtf16Encoding";
+    private const string KeyVersion = "Version";
+    private const string KeyPooling = "Pooling";
+    private const string KeyReadOnly = "Read Only";
+    private const string KeyDataSource = "Data Source";
+    private const string KeyUri = "Uri";
+    private const string KeyFullUri = "FullUri";
+    private const string KeySynchronous = "Synchronous";
+    private const string KeyDefaultTimeOut = "Default Timeout";
+    private const string KeyBusyTimeOut = "BusyTimeout";
+    private const int DefaultVersion = 3;
+    private const int DefaultDefaultTimeOut = 30;
+    private const int DefaultBusyTimeOut = 0;
+    private const string DefaultDataSource = "";
+    private const string DefaultUri = null;
+    private const string DefaultFullUri = null;
+    private const bool DefaultPooling = false;
+    private const bool DefaultReadOnly = false;
+    private const SynchronizationModes DefaultSynchronous = SynchronizationModes.Normal;
+    private const bool DefaultUseUtf16Encoding = false;
+
+    private T GetValue<T>(string key, T defaultValue)
+    {
+      object value;
+      if (!TryGetValue(key, out value))
+      {
+        return defaultValue;
+      }
+
+      // int/uint
+      if (typeof(T) == typeof(int) ||
+          typeof(T) == typeof(uint))
+      {
+        return (T) Convert.ChangeType(
+          Convert.ToInt32(value, CultureInfo.CurrentCulture),
+          typeof(T));
+      }
+
+      // string
+      if (typeof(T) == typeof(string))
+      {
+        if (value == null)
+        {
+          return defaultValue;
+        }
+
+        if (value is string)
+        {
+          return (T) Convert.ChangeType(value, typeof(T));
+        }
+      }
+
+      // bool
+      if (typeof(T) == typeof(bool))
+      {
+        if (value == null)
+        {
+          return defaultValue;
+        }
+
+        bool returnValue;
+        if (value is string)
+        {
+          var s = Convert.ToString(value).ToLower();
+          switch (s)
+          {
+            case "true":
+              returnValue = true;
+              break;
+
+            case "false":
+              returnValue = false;
+              break;
+
+            default:
+              returnValue = s != "0";
+              break;
+          }
+        }
+        else if (value is bool)
+        {
+          returnValue = (bool) value;
+        }
+        else
+        {
+          returnValue = Convert.ToInt64(value) != 0;
+        }
+
+        return (T) Convert.ChangeType(returnValue, typeof(T));
+      }
+
+      if (typeof(T) == typeof(SynchronizationModes))
+      {
+        SynchronizationModes returnValue;
+        if (value is string)
+        {
+          try
+          {
+            returnValue = (SynchronizationModes) GetConverter(typeof(SynchronizationModes)).ConvertFrom(value);
+          }
+          catch
+          {
+            return defaultValue;
+          }
+        }
+        else
+        {
+          returnValue = (SynchronizationModes) value;
+        }
+
+        return (T) Convert.ChangeType(returnValue, typeof(T));
+      }
+
+      // not sure what this is...
+      throw new InvalidCastException("Unable to cast to this data type.");
+    }
+
+    /// <summary>
+    /// If we are using utf16 encoding
+    /// </summary>
+    // ReSharper disable once InconsistentNaming
+    public bool UseUTF16Encoding
+    {
+      get { return GetValue(KeyUseUtf16Encoding, DefaultUseUtf16Encoding); }
+      set { this[KeyUseUtf16Encoding] = value; }
+    }
+
+    /// <summary>
+    /// Use Connection pooling or not.
+    /// </summary>
+    public bool Pooling
+    {
+      get { return GetValue(KeyPooling, DefaultPooling); }
+      set { this[KeyPooling] = value; }
+    }
+
+    /// <summary>
+    /// Get if the database is in readonly mode or not.
+    /// </summary>
+    public bool ReadOnly
+    {
+      get { return GetValue(KeyReadOnly, DefaultReadOnly);}
+      set { this[KeyReadOnly] = value; }
+    }
+
+    /// <summary>
+    /// Get the version number
+    /// </summary>
+    public int Version
+    {
+      get { return GetValue(KeyVersion, DefaultVersion); }
+      set
+      {
+        if (value != DefaultVersion)
+        {
+          throw new NotSupportedException();
+        }
+
+        this["version"] = value;
+      }
+    }
+
+    /// <summary>
+    /// Gets the default command timeout.
+    /// </summary>
+    public int DefaultTimeout
+    {
+      get { return GetValue(KeyDefaultTimeOut, DefaultDefaultTimeOut); }
+      set { this[KeyDefaultTimeOut] = value; }
+    }
+
+    /// <summary>
+    /// Get the busy timeout to stop long queries.
+    /// </summary>
+    public int BusyTimeout
+    {
+      get { return GetValue(KeyBusyTimeOut, DefaultBusyTimeOut); }
+      set { this[KeyBusyTimeOut] = value; }
+    }
+
+    /// <summary>
+    /// Get the Synchronization Mode
+    /// </summary>
+    public SynchronizationModes SyncMode
+    {
+      get { return GetValue(KeySynchronous, DefaultSynchronous); }
+      set { this[KeySynchronous] = value; }
+    }
+
     /// <summary>
     /// Gets/Sets the filename or memory to open on the connection string.
     /// </summary>
     public string DataSource
     {
-      get
-      {
-        TryGetValue("data source", out var value);
-        return value?.ToString();
-      }
-      set => this["data source"] = value;
+      get { return GetValue(KeyDataSource, DefaultDataSource); }
+      set { this[KeyDataSource] = value; }
+    }
+
+    /// <summary>
+    /// Alternate Data source.
+    /// </summary>
+    public string Uri
+    {
+      get { return GetValue(KeyUri, DefaultUri); }
+      set { this[KeyUri] = value; }
+    }
+
+    /// <summary>
+    /// Alternate Data source.
+    /// </summary>
+    public string FullUri
+    {
+      get { return GetValue(KeyFullUri, DefaultFullUri); }
+      set { this[KeyFullUri] = value; }
     }
   }
 }
