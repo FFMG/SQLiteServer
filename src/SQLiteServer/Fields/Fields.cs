9eb86ab19ad77cf7beb1bcc248e0202d4b58c4c8
FFMG <github@myoddweb.com>
2018-05-06 20:47:48 +0200

    Added string > null to field as well as List<T> to field as well

--- src/SQLiteServer/Fields/Fields.cs
+++ src/SQLiteServer/Fields/Fields.cs
@@ -72,7 +72,7 @@ namespace SQLiteServer.Fields
       var typeOfT = typeof(T);
       return typeOfT.IsValueType || typeOfT.GetConstructor(Type.EmptyTypes) != null;
     }
-
+    
     /// <summary>
     /// Parse an object into all the 'Fields'
     /// </summary>
@@ -90,14 +90,14 @@ namespace SQLiteServer.Fields
       {
         throw new ArgumentException(nameof(value));
       }
-
+      
       var fields = new Fields();
 
       // add all the items.
       foreach (var field in typeof(T).GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public)
         .Where(f => f.GetCustomAttribute<CompilerGeneratedAttribute>() == null))
       {
-        fields.Add(  field.Name, field.FieldType, field.GetValue(value) );
+        fields.Add(field.Name, field.FieldType, field.GetValue(value));
       }
       return fields;
     }
@@ -114,7 +114,8 @@ namespace SQLiteServer.Fields
         throw new FieldsException($"The variable type, {nameof(T)}, does not have a default constructor");
       }
 
-      object result = Activator.CreateInstance<T>();
+      // create the instance
+      var result = Activator.CreateInstance<T>();
 
       // add all the items.
       foreach (var field in _fields )
@@ -124,9 +125,9 @@ namespace SQLiteServer.Fields
         {
           continue;
         }
-        fi.SetValue(result, field.Value );
+        fi.SetValue(result, field.Get<object>() );
       }
-      return (T)result;
+      return result;
     }
     
     /// <summary>
